99f56284689610d29ab470f60c7a948aec882c0e
Frederick Kaucher <frederick.kaucher@gmail.com>
2021-09-23 13:41:19 -0600

    finish Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args){
+    double num1 = 15;
+    double num2 = 6;
+    double num3 = 2;
+    double avg = 0.0;
+    avg = (num1 + num2 + num3)/3;
+    System.out.println(avg);
+  }
+}
