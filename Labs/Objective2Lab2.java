8e43dfd192fc1347842e6c7f6f9e08204bb3cc23
Frederick Kaucher <frederick.kaucher@gmail.com>
2021-09-23 11:55:45 -0600

    finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Frederick Kaucher";
+    int age = 38;
+    String state = "Florida";
+    System.out.println("Name: " + name + "   " + "Age: " + age +"   " + "State: " + state);
+
+  }
+}
