dcad1bf7ac19e687426dd324dc4a87427a64bb23
Frederick Kaucher <frederick.kaucher@gmail.com>
2021-10-04 18:49:16 -0600

    finish Objective5Lab3

---
+++ Objective5Lab3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Objective5Lab3 {
+
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int userNum;
+
+      System.out.println("Please enter a number: ");
+      userNum = scanner.nextInt();
+      if (userNum > 0) {
+        System.out.println( userNum + " is positve");
+      }
+      else {
+        System.out.println( userNum + " is negative");
+      }
+
+      scanner.close();
+  }
+}
